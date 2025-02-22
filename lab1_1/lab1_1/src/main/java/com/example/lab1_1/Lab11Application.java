db2755b25f805babbbafa2185cc720631478747b
Sarina2405 <196175678+Sarina2405@users.noreply.github.com>
2025-02-23 02:14:47 +0530

    Add files via upload

---
+++ lab1_1/lab1_1/src/main/java/com/example/lab1_1/Lab11Application.java
@@ -0,0 +1,12 @@
+package com.example.lab1_1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
